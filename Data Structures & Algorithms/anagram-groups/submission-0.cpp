028fb5e84f1a2e375abf4858c6bfc5bf0dfee43c
Shakeela Mehrat <b24ee1071@iitj.ac.in>
2026-09-23 03:51:51 +0530

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>> result;
+        unordered_map<string, vector<string>> hash;
+        string sorted;
+        for(int i=0; i<strs.size(); i++){
+            sorted= strs[i];
+            sort(sorted.begin(), sorted.end());
+            hash[sorted].push_back(strs[i]);
+        }
+        for(auto it: hash){
+            result.push_back(it.second);
+        }
+        return result;
+    }
+};
