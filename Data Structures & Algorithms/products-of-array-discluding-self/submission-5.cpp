cd897cda855fae896a42546006ee5e5e2fddbd9b
Shakeela Mehrat <b24ee1071@iitj.ac.in>
2026-09-24 06:36:18 +0530

    Add: products-of-array-discluding-self - submission-5

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-5.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n= nums.size();
+        vector<int> result(n, 1);
+        for(int i=1; i<n; i++){
+            result[i]= result[i-1]*nums[i-1];
+        }
+        int postfix=1;
+        for(int i=n-1; i>=0; i--){
+            result[i]*=postfix;
+            postfix*=nums[i];
+        }
+        return result;
+    }
+};
